44c20d5c371626f18550062afc3edd25ee5116c3
scollo26 <giovanniscollo26@gmail.com>
2021-11-19 16:31:56 +0100

    ciclo-while

--- js/script.js
+++ js/script.js
@@ -5,14 +5,26 @@
 // va applicato uno sconto del 40% per gli over 65.
 
 
+let kilometri;
+let i= 0;
+while(isNaN(kilometri)){
+  if ( i < 3){
+    kilometri =parseInt(prompt('Inserisci kilometri'));
+    i++;
+  }
+    else{
+    kilometri =parseInt(prompt('Testo sbagliato'));
+  }
+}
 
-
-const kilometri =parseInt(prompt('inserisci kilometri'));
-const eta = parseInt(prompt('inserisci eta'));
+const eta = parseInt(prompt('Inserisci eta'));
 // console.log(kilometri, eta);
 
 let prezzoBiglietto= kilometri * 0.21;
 // console.log(prezzoBiglietto);
+
+
+
 
 
 if (eta <= 17 ) {
